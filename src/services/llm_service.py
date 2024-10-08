e28fb921604479389bc6f4b85dcb32ce3dbb8f71
Bryan <bryan.amrein@gmx.ch>
2024-10-08 09:41:59 -0700

    Better openAI prompting

--- src/services/llm_service.py
+++ src/services/llm_service.py
@@ -8,12 +8,23 @@ api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=api_key)
 
 test = '''def test_add_positive_numbers(self):self.assertEqual(add(2, 3), 5)'''
+test2 = '''abdalkjflkasf'''
 
 def generate_implementation(test: str):
     completion = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[
-        {"role": "system", "content": "You are a programming helper and you only write code and no other text"},
-        {"role": "user", "content": "Write the implementation for the testcase " + test}]
+        {"role": "system", "content": "You are an experienced programmer with plenty of knowledge in testing"},
+        {"role": "user", "content": """"
+        First check if the following input is code. 
+        Then ckeck if the code is a valid testcase in the appropriate language. 
+        Then check if the test has any syntax errors.
+        If any of the 3 check are not the case return a short error message.
+
+        After the checks, if its a valid testcase, create the method which will solve the testcase input.
+        Return the generated method without any extra text
+
+        Input: """ + test}]
     )
     return completion;
+
