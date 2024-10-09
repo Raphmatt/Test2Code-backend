76b6c73407abce6277c6c03f7d20214983af34d4
Raphael <raphael.andermatt@gmail.com>
2024-10-09 15:58:54 -0700

    add cors middleware

--- src/main.py
+++ src/main.py
@@ -1,11 +1,31 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 import dotenv
 
 from router import router
 
 dotenv.load_dotenv()
+
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router=router)
 
 
